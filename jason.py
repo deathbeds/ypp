f5cea8a6cf1d4c51e1d93616d5e1ffaea2dff7f3
tonyfast <tony.fast@gmail.com>
2019-03-20 16:58:29 -0400

    tight up jason

--- jason.py
+++ jason.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# '''`jason` is a symbollic language for the JSON family.  Currently, `jason` provides methods for
+# converting strings in different formats (eg. Toml, Yaml, Csv, Graphviz) and symbollic operations for:
+#
+# * jsonpatch
+# * jsonpointer
+# * jsonld
+# * jsondiff
+# * jsonschema
+# '''
+
 # Standard Library
 import abc
-import collections
 import functools
 import io
 
@@ -22,7 +31,6 @@ __all__ = (
     "Csv",
     "Context",
     "Schema",
-    "Patch",
     "T",
     "Toml",
     "Y",
@@ -34,22 +42,17 @@ if __name__ == "__main__":
     get_ipython = IPython.get_ipython
 
 
-list_types = list, collections.UserList
-str_types = str, collections.UserString
-dict_types = dict, collections.UserDict
-
-
 class Jay(abc.ABCMeta):
     """Base class for short shortened string formatters."""
 
     def __mod__(Jay, object, callable=None):
         object = (callable or Jay.load)(object)
         if not isinstance(object, J):
-            if isinstance(object, (dict, collections.UserDict)):
+            if isinstance(object, dict):
                 return D(object)
-            if isinstance(object, (list, collections.UserList)):
+            if isinstance(object, list):
                 return L(object)
-            if isinstance(object, (str, collections.UserString)):
+            if isinstance(object, str):
                 return S(object)
         return object
 
@@ -64,18 +67,14 @@ class J(metaclass=Jay):
     ...
 
 
-shell = get_ipython()
-
-
-shell.transform_cell("!echo stuff")
-
-
 class Echo(J):
     '''echo "{}"'''
 
     @classmethod
     def load(Bang, str):
-        return IPython.get_ipython().system(Bang.__doc__.splitlines()[0].format(str))
+        return "\n".join(
+            IPython.get_ipython().system(Bang.__doc__.splitlines()[0].format(str))
+        )
 
 
 class Coffee(Echo):
@@ -97,9 +96,12 @@ class Json(J):
 
 
 class Csv(J):
-    """>>> Csv('''a, b, c
-... 1, 2, 3''')
-[['a', ' b', ' c'], ['1', ' 2', ' 3']]
+    """>>> Csv('''a,b,c
+... 1,2,3''')
+[['a', 'b', 'c'], ['1', '2', '3']]
+>>> Csv%(' ','''a b c
+... 1 2 3''')
+[['a', 'b', 'c'], ['1', '2', '3']]
 """
 
     @staticmethod
@@ -128,70 +130,82 @@ class Cfg(J):
 class Object:
     def __getitem__(O, object):
         if isinstance(object, str) and object.startswith("/"):
-            return J(__import__("jsonpointer").resolve_pointer(O.data, object))
+            return J(__import__("jsonpointer").resolve_pointer(O, object))
         return super().__getitem__(object)
 
-    def __add__(O, object, *, op="add"):
-        if not isinstance(object, Patch):
-            object = Patch(
+    def __attr__(O, object, *, attr=None, op="add", in_place=False):
+        import jsonpatch
+
+        if not isinstance(object, jsonpatch.JsonPatch):
+            if isinstance(object, str):
+                if isinstance(O, str):
+                    return S(getattr(super(), f"__{attr}__")(object))
+                object = object.split()
+            if isinstance(object, list):
+                object = dict(zip(object, [None] * len(object)))
+            object = jsonpatch.apply_patch(
+                O,
                 [
                     {
                         "op": op,
-                        "path": key,
+                        "path": ["/", ""][key.startswith("/")] + key,
                         **({} if op == "replace" else {"value": value}),
                     }
                     for key, value in (
                         object.items()
                         if isinstance(object, dict)
-                        else zip(object, [""] * len(object))
+                        else zip(object, [None] * len(object))
                     )
-                ]
+                ],
+                in_place=in_place,
             )
-        return object(O.data)
+        return object
 
-    __floordiv__ = functools.partialmethod(__add__, op="replace")
-
-    __sub__ = functools.partialmethod(__add__, op="remove")
+    __add__ = functools.partialmethod(__attr__, attr="add", in_place=True)
+    __iadd__ = functools.partialmethod(__attr__, attr="iadd", in_place=True)
+    __sub__ = functools.partialmethod(__attr__, attr="sub", op="remove")
+    __isub__ = functools.partialmethod(
+        __attr__, attr="isub", op="remove", in_place=True
+    )
+    __truediv__ = functools.partialmethod(__attr__, attr="floordiv", op="replace")
+    __itruediv__ = functools.partialmethod(
+        __attr__, attr="ifloordiv", op="replace", in_place=True
+    )
 
     def __or__(O, object):
-        return __import__("jsondiff").diff(O.data, object)
+        return __import__("jsondiff").diff(O, object)
 
     def __and__(O, object):
-        O.update({"@context": object})
-        return O
-
-    __truediv__ = __floordiv__
+        return O.update({"@context": object}) or O
 
     def __matmul__(O, object):
-        return Context(object) @ O.data
+        return Context(object) @ O
 
     def __mod__(O, object):
-        return Context(object) % O.data
+        return Context(object) % O
 
     def __call__(O, object):
-        return Schema(object)(O.data)
+        return Schema(object)(O)
 
-    def __setitem__(J, path, value):
+    def __setitem__(O, path, value):
         if path.startswith("/"):
-            return J + {path: value}
+            O += {path: value}
+            return O
         return super().__setitem__(path, value)
 
-    def __wrapped__(O):
-        raise AttributeError("__wrapped__")
-
     def __pos__(O):
-        data = dict(O.data)
+        data = dict(O)
         return Context(data.pop("@context")) @ data
 
     def __neg__(O):
-        data = dict(O.data)
+        data = dict(O)
         return Context(data.pop("@context")) % data
 
 
 O = Object
 
 
-class String(O, collections.UserString):
+class String(O, str):
     """>>> s = S("foo")
 >>> type(s) # doctest: +ELLIPSIS
 <...S...>
@@ -205,7 +219,7 @@ class String(O, collections.UserString):
 S = String
 
 
-class List(O, collections.UserList):
+class List(O, list):
     """>>> l = L(['a', 'foo'])
 >>> type(l) # doctest: +ELLIPSIS
 <...L...>
@@ -226,25 +240,7 @@ class List(O, collections.UserList):
 L = List
 
 
-class Patch(__import__("jsonpatch").JsonPatch, L):
-    """>>> l = L(['a', 'foo'])
->>> type(l) # doctest: +ELLIPSIS
-<...L...>
->>> l['/0']
-'a'
->>> l['/1/1']
-'o'
-"""
-
-    def __call__(Patch, object):
-        return J(Patch.apply(object))
-
-    @property
-    def data(Patch):
-        return Patch.patch
-
-
-class Dict(O, collections.UserDict):
+class Dict(O, dict):
     """>>> d = Json%'{"a": [1, {"b": ["foo", 3, "bar"]}]}'
 >>> d
 {'a': [1, {'b': ['foo', 3, 'bar']}]}
@@ -260,14 +256,8 @@ class Dict(O, collections.UserDict):
 [{'https://foo': [{'@value': 'testing'}]}]
 """
 
-    def __deepcopy__(D, state):
-        return type(D)(D.data)
-
 
 D = Dict
-
-
-d = Json % '{"a": [1, {"b": ["foo", 3, "bar"]}]}'
 
 
 class Schema(D):
@@ -279,7 +269,7 @@ class Schema(D):
 """
 
     def __call__(Schema, object):
-        __import__("jsonschema").validate(object, Schema.data)
+        __import__("jsonschema").validate(object, Schema)
         return object
 
 
@@ -292,12 +282,12 @@ class Context(D):
 """
 
     def __mod__(Context, object):
-        return D(__import__("pyld").jsonld.compact(object, Context.data))
+        return D(__import__("pyld").jsonld.compact(object, Context))
 
     def __matmul__(Context, object):
         if isinstance(object, str):
-            return Context.expand(object, Context.data)
-        object.update({"@context": Context.data.get("@context", Context.data)})
+            return Context.expand(object, Context)
+        object.update({"@context": Context.get("@context", Context)})
         return L(__import__("pyld").jsonld.expand(object))
 
     __call__ = __matmul__
@@ -315,28 +305,42 @@ class Context(D):
         return str
 
 
-class Yaml(J):
-    """>>> Y%'[a, b]'
-['a', 'b']
->>> Y%'{a: b}'       
-{'a': 'b'}
-"""
-
-    @staticmethod
-    def load(str):
-        import ruamel.yaml
-
-        return ruamel.yaml.load(io.StringIO(str), Loader=ruamel.yaml.Loader)
-
-
-Y = Yaml
-
-
 class Graphviz(J):
     load = staticmethod(lambda str: __import__("graphviz").Source(str))
 
 
 G = Graphviz
+
+
+class Yaml(J):
+    """>>> Y%'[a, b]'
+['a', 'b']
+>>> y = Y%'{a: b}'
+>>> y
+{'a': 'b'}
+>>> y += {'c': 1, 'd': 5}
+>>> y
+{'a': 'b', 'c': 1, 'd': 5}
+>>> y - {'c': None}
+{'a': 'b', 'd': 5}
+>>> assert y == {'a': 'b', 'c': 1, 'd': 5}
+>>> y -= {'c': None}
+>>> y
+{'a': 'b', 'd': 5}
+>>> assert not y - "a d"
+"""
+
+    @staticmethod
+    def load(str):
+        try:
+            import ruamel.yaml as yaml
+        except:
+            import yaml
+
+        return yaml.safe_load(io.StringIO(str))
+
+
+Y = Yaml
 
 
 class Toml(J):
@@ -351,7 +355,14 @@ class Toml(J):
 
 T = Toml
 
-
+#     Csv%"""\
+#     add, patch add
+#     sub, patch remove
+#     truediv, patch replace
+#     call, schema validate
+#     or, diff
+#     and, inline context
+#     """
 if __name__ == "__main__":
     import pidgin, nbconvert, black, IPython
 
@@ -378,4 +389,6 @@ if __name__ == "__main__":
         get_ipython().system("pyflakes jason.py")
     __import__("doctest").testmod()
 
-# https://mybinder.org/v2/gh/deathbeds/ypp/a4c56e9a3a761b32cb3c9d5b630f22f4a216dd67?filepath=examples.ipynb
+
+# '''https://mybinder.org/v2/gh/deathbeds/ypp/a4c56e9a3a761b32cb3c9d5b630f22f4a216dd67?filepath=examples.ipynb
+# '''
